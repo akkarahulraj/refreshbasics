2354e592d379dc286ddd2756b560a10721a5b024
rahul <rahulrajincredible@gmail.com>
2021-05-16 19:49:30 +0530

    added1

--- basics/multiplewindows.py
+++ basics/multiplewindows.py
@@ -3,4 +3,9 @@
 from selenium import webdriver
 
 driver= webdriver.Chrome(executable_path="C:\chromedriver_win32\chromedriver.exe")
-driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+driver.get("https://the-internet.herokuapp.com/windows")
+driver.find_element_by_link_text("Click Here").click()
+
+childwindow= driver.window_handles[1]
+driver.switch_to.window(childwindow)
+print(driver.find_element_by_tag_name("h3").text)
